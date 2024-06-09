c2fb70b6021258a80978c29afb4029892b420565
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-09 03:30:55 -0700

    instantiated app and DB connection

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const path = require('path');
 const cors = require('cors');
 const corsOptions = require('./config/cors.config');
 const PORT = process.env.PORT || 3500;
+
+// Instantiate app
+const app = express();
+require('./config/mongoose.config');
