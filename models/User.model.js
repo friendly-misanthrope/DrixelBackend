5a7283517d04caa91992fa5a61eaf429852740af
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-09 02:40:15 -0700

    added email field to User schema

--- models/User.model.js
+++ models/User.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const { isEmail, isAlphanumeric,  } = require('validator')
+const { isEmail  } = require('validator')
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -9,6 +9,13 @@ const UserSchema = new Schema({
     required: [true, "Username is required"],
     minLength: [4, "Username must be at least 4 characters"],
     maxLength: [16, "Username must be between 4 and 16 characters"]
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    minLength: [4, "Email must be at least characters"],
+    maxLength: [64, "Email must be between 4 and 64 characters"],
+    validate: [isEmail, "Please enter a valid email address"]
   }
 })
 
