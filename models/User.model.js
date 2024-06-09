5c9e9e59ea4f4b88ab6fa041c4432db9a8e4bfda
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-09 02:47:52 -0700

    added albums field to User schema

--- models/User.model.js
+++ models/User.model.js
@@ -22,6 +22,10 @@ const UserSchema = new Schema({
     required: [true, "Password is required"],
     minLength: [8, "Password must be at least 8 characters"],
     maxLength: [64, "Password must be between 8 and 64 characters"]
-  }
+  },
+  albums: [{
+    type: Schema.Types.ObjectId,
+    ref: "Album"
+  }]
 })
 
