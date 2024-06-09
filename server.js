2537c4d7492b85facecb493dd466aa6bff247a83
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-09 03:28:00 -0700

    defined initial imports

---
+++ server.js
@@ -0,0 +1,6 @@
+/* IMPORTS */
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const corsOptions = require('./config/cors.config');
+const PORT = process.env.PORT || 3500;
