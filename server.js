1b938cd3687cf8e1f27639897493b353587271dc
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-09 04:02:34 -0700

    applied cookieParser, CORS, and bodyParser middleware

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const corsOptions = require('./config/cors.config');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3500;
 
 // Instantiate app
 const app = express();
 require('./config/mongoose.config');
+
+/* THIRD-PARTY MIDDLEWARE */
+app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({extended: true}));
