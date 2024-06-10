751e04fca32ae739eb0e3f521474559e3df6d3cc
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-10 00:49:43 -0700

    fixed bug where .exec() was causing mongoose queries to fail

--- controllers/auth/register.controller.js
+++ controllers/auth/register.controller.js
@@ -2,52 +2,58 @@ const Users = require('../../models/User.model');
 
 //* Register User
 const registerUser = async (req, res) => {
+
   // Check for existing user in DB
   try {
     const potentialUser = await Users.findOne({
       $or: [
-        { email: req.body.email },
-        { username: req.body.username }
+        { username: req.body.username },
+        { email: req.body.email }
       ]
-    }).exec();
+    });
+
     // If email or username exist, return 409 conflict
-    if (potentialUser.email) {
+    if (potentialUser.username) {
       return res.status(409).json({
-        message: `An account with email ${potentialUser.email} already exists.`
+        message: `An account with email ${potentialUser.username} already exists.`
       });
-    } else if (potentialUser.username) {
+    } else if (potentialUser.email) {
       return res.status(409).json({
-        message: `An account with username ${potentialUser.username} already exists.`
+        message: `An account with username ${potentialUser.email} already exists.`
       });
     }
   } catch (e) {
-    res.status(500).json({
+    return res.status(500).json({
       message: "Unable to check DB for existing user",
       error: e
     });
   }
-  // Destructure user fields from request body
-  const {
-    username,
-    email,
-    password,
-    confirmPassword
-  } = req.body;
+
+
   // Create new user from req.body
   try {
+    // Destructure user fields from request body
+    const {
+      username,
+      email,
+      password,
+      confirmPassword
+    } = req.body;
+
     const newUser = await Users.create({
       username,
       email,
       password,
       confirmPassword
-    }).exec();
+    });
+
     // Send status code 201 & new user document in response 
-    res.status(201).json({
+    return res.status(201).json({
       message: `User ${newUser.username} registered successfully.`,
       newUser
     });
   } catch (e) {
-    res.status(500).json({
+    return res.status(500).json({
       message: "Unable to create new user",
       error: e
     });
